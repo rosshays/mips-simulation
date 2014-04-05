1309c26153e99da9734bfae92ce6c18e3ffad6e1
rjkucia92 <rjkucia@yahoo.com>
2014-04-05 14:51:38 -0400

    resizes window and boxes, adds stack box

--- application.py
+++ application.py
@@ -12,16 +12,25 @@ class MIPSApplication(tk.Frame):
     def create_widgets(self):
         ui.create_menu(self, root)
         ui.create_file(self, root)
-        ui.create_input(self, "grey")
-        ui.create_bin(self, "grey")
+        
+        ui.create_input(self)
+        ui.create_bin(self)
+        ui.create_stack(self)
         ui.lock_text(self)
+        
         button_panel = ui.create_control(self)
         
         ui.adjust_layout(self)
         ui.arrange_control(self, button_panel)
 
     def step_once(self):
-        print("hey")
+        print(">Stepping once")
+    
+    def run_prog(self):
+	    print(">Running program")
+		
+    def stop_prog(self):
+	    print(">Stopping program")
 
     def load_input_file(self):
         filename = lf.get_file(self)
